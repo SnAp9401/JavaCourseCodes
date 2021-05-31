bb1032d31a720ad75c0d4993993a556c842cafa5
esiz <614972486@qq.com>
2021-05-31 09:36:18 +0800

    week4

---
+++ src/main/java/com/esiz/week4/mission3/ResultEntity.java
@@ -0,0 +1,13 @@
+package com.esiz.week4.mission3;
+
+public class ResultEntity {
+    private int result;
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/com/esiz/week4/mission3/Week04Application.java
@@ -0,0 +1,13 @@
+package com.esiz.week4.mission3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week04Application.class, args);
+	}
+
+}
